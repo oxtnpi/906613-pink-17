2cbca414a1e7aa7e06b2687de36a274967f9d3af
Anastasiya Turenko <oxtnpi@gmail.com>
2019-09-07 18:49:13 +0300

    Исправляет подключение normalize

--- gulpfile.js
+++ gulpfile.js
@@ -86,6 +86,7 @@ gulp.task("copy",function(){
     "source/fonts/**/*.{woff,woff2}",
     "source/img/**",
     "source/js/**",
+    "source/css/normalize.css",
     "source/*.ico"
   ], {
     base:"source"
